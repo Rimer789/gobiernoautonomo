cc1a91fbd80e460c58335431985c4934dbb97292
saulrosa <practicaempresarial2024@gmail.com>
2024-05-07 16:10:57 -0400

    rejoc deli

--- src/components/referencias/Footer.jsx
+++ src/components/referencias/Footer.jsx
@@ -11,47 +11,40 @@ const Footer = () => {
       try {
         const response = await fetch('http://worldtimeapi.org/api/timezone/America/La_Paz');
         const data = await response.json();
-        const horaBolivia = new Date(data.datetime);
-        const horaBoliviaLocal = horaBolivia.toLocaleString("en-US", { timeZone: "America/La_Paz" });
-        console.log('Hora actual en Bolivia:', horaBoliviaLocal);
-        const horaActual = new Date(horaBoliviaLocal);
-        const horaInicioManana = new Date(horaBoliviaLocal);
-        horaInicioManana.setHours(8, 0, 0); // 8:00 AM
-        const horaFinManana = new Date(horaBoliviaLocal);
-        horaFinManana.setHours(12, 0, 0); // 11:36 AM
-        const horaInicioTarde = new Date(horaBoliviaLocal);
-        horaInicioTarde.setHours(13, 30, 0); // 1:30 PM
-        const horaFinTarde = new Date(horaBoliviaLocal);
-        horaFinTarde.setHours(17, 30, 0); // 5:30 PM
-        const horaFinDiaSiguiente = new Date(horaBoliviaLocal);
-        horaFinDiaSiguiente.setDate(horaFinDiaSiguiente.getDate() + 1);
-        horaFinDiaSiguiente.setHours(8, 0, 0); // 8:00 AM
-
-        if ((horaActual >= horaInicioManana && horaActual <= horaFinManana) || (horaActual >= horaInicioTarde && horaActual <= horaFinTarde)) {
-          setStatus("Abierto");
-        } else if (horaActual >= horaInicioTarde && horaActual <= horaFinTarde) {
-          setStatus("Abierto");
+        const horaBoliviaLocal = new Date(data.datetime);
+        const horaActual = horaBoliviaLocal.getHours();
+        const horaInicioManana = 8;
+        const horaFinManana = 12;
+        const horaInicioTarde = 13;
+        const horaFinTarde = 17;
+  
+        if ((horaActual >= horaInicioManana && horaActual < horaFinManana) || (horaActual >= horaInicioTarde && horaActual < horaFinTarde)) {
+          setStatus("Abierto ahora");
         } else {
-          setStatus("Cerrado");
+          setStatus("Cerrado ahora");
         }
-        console.log('Status:', status);
       } catch (error) {
         console.error('Error al obtener la hora:', error);
         setStatus("Desconocido");
       }
     };
-
+  
     obtenerHoraActual();
   }, []);
+  
+  useEffect(() => {
+    console.log('Status:', status); // Esta consola mostrará el estado actualizado
+  }, [status]);
+  
 
   return (
     <footer className="f-wrapper">
-      <div className="dividir">
+      <div className="dividir"> 
         <div className="footer-section">
           <div className="title">Gobierno Autónomo Municipal de Punata</div>
           <div className="description">Oficina central: Calle Sucre entre calle Antofagasta y calle Rafael Urquidi</div>
           <div className="description">Lunes-Viernes, de 8:00 AM a 12:00 PM y de 1:30 PM a 5:30 PM</div>
-          <div style={{ fontSize: "20px", color: status === "Abierto" ? "#4CAF50" : "#FF5733" }}>{status}</div>
+          <div style={{ fontSize: "20px", color: status === "Abierto ahora" ? "#4CAF50" : "#FF5733" }}>{status}</div>
         </div>
         <div className="image-container">
           <img src={mediumImage} alt="Imagen Mediana" className="medium-image" />
